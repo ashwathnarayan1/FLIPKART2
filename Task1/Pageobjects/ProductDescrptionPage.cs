e6a390e2ff4e30ef35edd97d441664d953f073e3
ashwa <eashwathnarayan@gmail.com>
2022-07-12 16:37:06 +0530

    My first commit

--- Task1/Pageobjects/ProductDescrptionPage.cs
+++ Task1/Pageobjects/ProductDescrptionPage.cs
@@ -1,5 +1,4 @@
 ﻿using OpenQA.Selenium;
-using SeleniumExtras.PageObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,23 +7,22 @@ using System.Threading.Tasks;
 
 namespace Task1.Pageobjects
 {
-    public class AddToCart
+    class ProductDescrptionPage
     {
         private IWebDriver driver;
-        public AddToCart(IWebDriver driver)
+        public ProductDescrptionPage (IWebDriver driver)
         {
             this.driver = driver;
-
         }
-
 
         private static By cart = By.XPath("//button[@class='_2KpZ6l _2U9uOA _3v1-ww']");
         private static By mycart = By.XPath("(//div[@class='_3g_HeN'])[1]");
+
         public void clickToAddToCart()
         {
             driver.FindElement(cart).Click();
-
         }
+
         public IWebElement getMycart()
         {
             return driver.FindElement(mycart);
