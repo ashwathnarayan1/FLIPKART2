14a75351f1c750f01d9f72b0e1cfe42a9ceecfad
ashwa <eashwathnarayan@gmail.com>
2022-07-14 12:38:43 +0530

    added screenshot when test fails

--- Task1/Utilities/Base.cs
+++ Task1/Utilities/Base.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
@@ -6,6 +9,7 @@ using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -17,7 +21,26 @@ namespace Task1.Utilities
 {
    public class Base
     {
+        public ExtentReports extent;
+        public ExtentTest test;
         public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            String reportPath = projectDirectory + "//index.html";
+            var htmlReporter = new ExtentHtmlReporter(reportPath);
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);          
+        }
+
+        [SetUp]
+        public void setup1()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
 
         [OneTimeSetUp]
         public void OpenBrowser()
@@ -53,6 +76,35 @@ namespace Task1.Utilities
                 driver = new EdgeDriver();
                 break;
             }
+        }
+
+        [TearDown]
+        public void afterTest()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
+           
+            DateTime time = DateTime.Now;
+            String fileName = "Screenshot_" + time.ToString("h_mm_ss") + ".png";
+           
+            if (status == TestStatus.Failed)
+            {
+                test.Fail("Test failed", captureScreenShot(driver, fileName));
+                test.Log(Status.Fail, "test failed with logtrace" + stackTrace);
+            }
+            else if (status == TestStatus.Passed)
+            {
+
+            }
+            extent.Flush();
+        }
+        public MediaEntityModelProvider captureScreenShot(IWebDriver driver, String screenShotName)
+
+        {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            var screenshot = ts.GetScreenshot().AsBase64EncodedString;
+
+            return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, screenShotName).Build();
         }
 
         [OneTimeTearDown]
